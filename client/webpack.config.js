a06a50c9103d1bb918e5a4e3b702c4d520360c98
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-09-27 10:01:05 -0400

    adds scripts to package.json, css loader and webpack. app is running.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,11 +19,49 @@ module.exports = () => {
     },
     plugins: [
       
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+        title: 'PWA_text_editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+        exclude: [/\.map$/, /manifest.*\.js(?:on)?$/, /index\.html$/],
+      }),
+      new WebpackPwaManifest({
+        name: 'PWA_text_editor',
+        short_name: 'PWA_text_editor',
+        description: 'PWA_text_editor',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        start_url: '/',
+        display: 'standalone',
+        publicPath: '/',
+        inject: true,
+        fingerprints: true,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons')
+          }
+          
+        ]
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.js$/i,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
       ],
     },
   };
